066ebeaca20377e19fdad92433a8d30b153dfe6d
balogun abdulmalik <balogunabdulmalik04@gmail.com>
2024-10-07 00:41:03 +0100

    corrected

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,54 +1,21 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+'''A module for working with Pascal's triangle.
+'''
 
-def factorial(n):
-    """
-    Calculate the factorial of a non-negative integer.
-
-    Args:
-        n (int): The number to calculate the factorial of.
-
-    Returns:
-        int: The factorial of n.
-
-    Raises:
-        ValueError: If n is negative.
-    """
-    if n < 0:
-        raise ValueError("Factorial is not defined for negative numbers")
-    if n == 0 or n == 1:
-        return 1
-    result = 1
-    for i in range(2, n + 1):
-        result *= i
-    return result
 
 def pascal_triangle(n):
+    '''Creates a list of lists of integers representing
+    the Pascal's triangle of a given integer.
     '''
-    Calculate pascal triangle.
-    
-    Args:
-        n (int): number of n rows of pascal triangle.
-    
-    Returns:
-        list: list of n row triangle
-    '''
-    store = list()
-    
-    if n <= 0:
-        return store
-    
-    for a in range(n):
-        # outer loop for n rows
-        r = 0
-        ans = list()
-        fact_a = factorial(a)
-        while r <= a:
-            sub = a - r
-            fact_a_r = factorial((sub))
-            fact_r = factorial(r)
-            store_ans = int(fact_a / ((fact_a_r) * fact_r))
-            ans.append(store_ans)
-            r += 1
-        store.append(ans)
-    
-    return store
+    triangle = []
+    if type(n) is not int or n <= 0:
+        return triangle
+    for i in range(n):
+        line = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                line.append(1)
+            elif i > 0 and j > 0:
+                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(line)
+    return triangle
